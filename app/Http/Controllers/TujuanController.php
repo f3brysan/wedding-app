9b1b762846914631ace72def98d51c031f811f9b
f3brysan <febrysan1995@gmail.com>
2023-07-03 07:16:28 +0700

    fix: space

--- app/Http/Controllers/TujuanController.php
+++ app/Http/Controllers/TujuanController.php
@@ -21,7 +21,7 @@ class TujuanController extends Controller
 Tanpa mengurangi rasa hormat. Kami mengundang Bapak/Ibu/Saudara/i '.$getData->nama.' untuk hadir pada acara pernikahan kami.
 
 Pada tanggal 15 Juli 2023,
-Bertempat di Gedung Serbaguna UNESA Ketintang Jl. Ketintang Pratama- Surabaya, Jawa Timur.
+Bertempat di Gedung Serbaguna UNESA Ketintang Jl. Ketintang Pratama, Surabaya, Jawa Timur.
 Berikut juga kami lampirkan undangan daring, Bapak/Ibu/Saudara/i '.$getData->nama.' dapat mengklik tautan berikut untuk menuju undangan pernikahan kami
 
 linaputri.febrysan.com/nikah/'.$getData->slug.'.
